92e9ea5c78fda51b16f90b8ee25ad518ac113ea6
Caio Aceti <caio.aceti@dextra-sw.com>
2020-05-11 10:23:16 -0300

    Creates PreventionTipsCoordinator

--- CoronaCases/CoronaCases/Presentation/PreventionTips/View/Enum/PreventionTips.swift
+++ CoronaCases/CoronaCases/Presentation/PreventionTips/View/Enum/PreventionTips.swift
@@ -18,15 +18,20 @@ enum PreventionTips: CaseIterable {
     var view: UIView {
         switch self {
         case .washHands:
-            return TipView(image: R.image.washHands() ?? UIImage(), tip: R.string.localizable.preventionTipsWashHands())
+            return TipView(image: R.image.washHands() ?? UIImage(),
+                           tip: R.string.localizable.preventionTipsWashHands())
         case .coverNoseAndMouth:
-            return TipView(image: R.image.coverNoseAndMouth() ?? UIImage(), tip: R.string.localizable.preventionTipsCoverNoseAndMouth())
+            return TipView(image: R.image.coverNoseAndMouth() ?? UIImage(),
+                           tip: R.string.localizable.preventionTipsCoverNoseAndMouth())
         case .avoidCloseContact:
-            return TipView(image: R.image.avoidCloseContactCustom() ?? UIImage(), tip: R.string.localizable.preventionTipsAvoidCloseContact())
+            return TipView(image: R.image.avoidCloseContactCustom() ?? UIImage(),
+                           tip: R.string.localizable.preventionTipsAvoidCloseContact())
         case .avoidTouchingFace:
-            return TipView(image: R.image.avoidTouchingFace() ?? UIImage(), tip: R.string.localizable.preventionTipsAvoidTouchingFace())
+            return TipView(image: R.image.avoidTouchingFace() ?? UIImage(),
+                           tip: R.string.localizable.preventionTipsAvoidTouchingFace())
         case .stayHome:
-            return TipView(image: R.image.stayHome() ?? UIImage(), tip: R.string.localizable.preventionTipsStayHome())
+            return TipView(image: R.image.stayHome() ?? UIImage(),
+                           tip: R.string.localizable.preventionTipsStayHome())
         }
     }
 }
